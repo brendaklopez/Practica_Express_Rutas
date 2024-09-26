988cc39c30c958363c70a9d5048d1347b5f0f257
Brenda <brendakeilalopez@gmail.com>
2024-09-26 18:59:16 -0300

    Se suben rtas de la ejer 2

--- server.js
+++ server.js
@@ -50,8 +50,103 @@ app.get('/api/lenguajes/frontend/:lenguaje', (req, res) => {
     res.status(200).send(filtrado);
 
 })
+//EJERCICIO 02.1
+app.get('/api/lenguajes/backend', (req, res) => {
+    res.send(JSON.stringify(infoLenguajes.backend));
+})
+//EJERCICIO 02.2
+app.get('/api/lenguajes/backend/:lenguaje', (req,res) => {
+    const lenguaje = req.params.lenguaje.toLocaleLowerCase();
+    console.log(`El parametro Backend recibido es: ${lenguaje}`)
+    const filtrado = infoLenguajes.backend.filter(
+        lenguajes => lenguajes.nombre.toLocaleLowerCase() === lenguaje
+    );
 
+    if (filtrado.length === 0){
+        return res.status(404).send(`No se encontró el curso con lenguaje: ${lenguaje}`)
+    }
+    res.status(200).send(filtrado);
+});
 
+//EJERCICIO 02.3
+
+app.get('/api/lenguajes/backend/turno/:turno', (req,res)=> {
+    const turno = req.params.turno.toLocaleLowerCase();
+    console.log(`El parametro de turno Backend es: ${turno}`)
+    const filtrado = infoLenguajes.backend.filter(
+        turnos => turnos.turno.toLocaleLowerCase() === turno
+    );
+    if (filtrado.length == 0 ){
+        return res.status(404).send(`No se encontró el curso con el turno: ${turno}`)
+    }
+    res.status(200).send(filtrado);
+});
+
+//EJERCICIO 02.4
+
+app.get('/api/lenguajes/frontend/turno/:turno', (req,res) =>{
+    const turno = req.params.turno.toLocaleLowerCase();
+    console.log(`El parametro de turno Frontend es: ${turno}`)
+    const filtrado = infoLenguajes.frontend.filter(
+        turnos => turnos.turno.toLocaleLowerCase() === turno
+    );
+
+    if (filtrado.length == 0){
+        return res.status(404).send(`No se encontro el curso con el turno: ${turno}`
+
+        )
+    }
+    res.status(200).send(filtrado);
+})
+
+//EJERCICIO 02.5
+app.get('/api/lenguajes/backend/cantidad/:cantidadAlumnos', (req,res) =>{
+    const cantidad = parseInt(req.params.cantidadAlumnos);
+    console.log(`El par cantidad de alumnos backend es: ${cantidad}`)
+    const filtrado = infoLenguajes.backend.filter(
+        alumnos => alumnos.cantidadAlumnos >= cantidad 
+    )
+    if (filtrado.length == 0){
+        return res.status(404).send(`No se encontro el curso de: ${cantidad}`
+
+        )
+    }
+    res.status(200).send(filtrado);
+    });
+
+//EJERCICIO 02.6
+app.get('/api/lenguajes/frontend/cantidad/:cantidadAlumnos', (req,res) => {
+    const cantidad = parseInt(req.params.cantidadAlumnos);
+    console.log(`El par cantidad de alumnos frontend es: ${cantidad}`)
+    const filtrado = infoLenguajes.backend.filter(
+        alumnos => alumnos.cantidadAlumnos >= cantidad 
+    )
+    if (filtrado.length == 0){
+        return res.status(404).send(`No se encontro el curso de: ${cantidad}`
+
+        )
+    }
+    res.status(200).send(filtrado);
+});
+//EJERCICIO 02.7
+/* 7) Generar un endpoint, con método GET, con la ruta /api/lenguajes/ 
+Que reciba por URL param, el parámetro "cantidadAlumnos". 
+Al invocar esta ruta del servidor, me debe traer TODOS los lenguajes que tengan la misma 
+cantidad de alumnos, o más, que la que se pasó por parámetro. */
+app.get('/api/lenguajes/cantidad/:cantidadAlumnos', (req,res) =>{
+    const cantidad = parseInt(req.params.cantidadAlumnos);
+    console.log(`El pararametro cantidad es: ${cantidad}`)
+    const ambosLenguajes = [...infoLenguajes.frontend, ...infoLenguajes.backend]
+    const filtrado = ambosLenguajes.filter(
+        alumnos => alumnos.cantidadAlumnos >= cantidad 
+    )
+    if (filtrado.length == 0){
+        return res.status(404).send(`No se encontro el curso de: ${cantidad}`
+
+        )
+    }
+    res.status(200).send(filtrado);
+});
 app.listen(PORT, HOSTNAME, () => {
     console.log(`El servidor está corriendo en http://${HOSTNAME}:${PORT}/`);
 });
